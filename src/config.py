978cb4a64d6554ec9c0d37f4f91fbc70b0426cac
crmckay55 <crmckay55@gmail.com>
2021-11-10 15:19:09 -0700

    moved all config variables to config.py

--- src/config.py
+++ src/config.py
@@ -2,23 +2,25 @@ import torch
 
 # Pytorch install: https://pytorch.org/get-started/locally/
 
-
+# untagged raw imaaged, and destination for modified images
 FILE_IN_PATH = 'F:/Python Projects/PyTorch-Valve-v1/model_pictures/originals_untagged/'
 FILE_OUT_PATH = 'F:/Python Projects/PyTorch-Valve-v1/model_pictures/originals_modified/'
+
+# input and output for inference
 VALIDATE_SOURCE = '../model_test/source'
 VALIDATE_PREDICTIONS = '../model_test/predictions'
 
-TRAIN_DIR =  '/model_pictures/train'            # training images and XML files directory
+# paths for model training
+TRAIN_DIR = '/model_pictures/train'            # training images and XML files directory
 MODEL_LOCATION = '../model_outputs/model100.pth'
 VALID_DIR = '/model_pictures/test'              # validation images and XML files directory
 
-
+# computational setup
 COMPUTATION_DEVICE = 'cuda'
 DEVICE = torch.device(COMPUTATION_DEVICE)
-
-BATCH_SIZE = 9  # increase / decrease according to GPU memeory
-RESIZE_TO = 640  # resize the image for training and transforms
-NUM_EPOCHS = 100  # number of epochs to train for
+BATCH_SIZE = 9  # increase / decrease according to GPU memeory - 9 works for GeForce 1070, 8GB RAM
+RESIZE_TO = 640  # resize the image for training and transforms - TODO: how big can we go?  is bigger better?
+NUM_EPOCHS = 100  # number of epochs to train for - TODO: how do epochs relate to training quality (output graphs) - other parameters?
 
 
 # classes: 0 index is reserved for background
